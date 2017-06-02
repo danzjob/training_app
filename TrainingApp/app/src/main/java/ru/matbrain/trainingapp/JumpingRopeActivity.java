eeea450c8d9f00426ded1cdc955e09ac918b7012
Danz <danzjob@yandex.ru>
2017-06-02 22:30:06 +0300

    Добавлена проверка на наличие браузера перед открытием подробной инструкции по упражнению

--- TrainingApp/app/src/main/java/ru/matbrain/trainingapp/JumpingRopeActivity.java
+++ TrainingApp/app/src/main/java/ru/matbrain/trainingapp/JumpingRopeActivity.java
@@ -1,13 +1,19 @@
 package ru.matbrain.trainingapp;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 /**
  * Created by danilzdanov on 24.05.17.
@@ -19,9 +25,10 @@ public class JumpingRopeActivity extends AppCompatActivity {
     private ImageButton minusButton;
     private ImageButton plusButton;
     private TextView jumpingRopeCountTV;
-//    private TextView descriptionTextView;
 
     private int jumpingRopeCount = 0;
+
+    private Button hrefButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,7 +39,7 @@ public class JumpingRopeActivity extends AppCompatActivity {
 
         jumpingRopeCount = getIntent().getIntExtra(MainActivity.JUMPING_ROPE_KEY, 0);
         jumpingRopeCountTV.setText(Integer.toString(jumpingRopeCount));
-
+        setHrefButtonBehavior();
 
     }
 
@@ -41,7 +48,7 @@ public class JumpingRopeActivity extends AppCompatActivity {
         minusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (jumpingRopeCount>=1){
+                if (jumpingRopeCount >= 1) {
                     jumpingRopeCount--;
                     jumpingRopeCountTV.setText(Integer.toString(jumpingRopeCount));
                 } else {
@@ -59,6 +66,8 @@ public class JumpingRopeActivity extends AppCompatActivity {
             }
         });
         jumpingRopeCountTV = (TextView) findViewById(R.id.repeats_count);
+
+        hrefButton = (Button) findViewById(R.id.href_jumping_rope);
     }
 
     @Override
@@ -68,4 +77,31 @@ public class JumpingRopeActivity extends AppCompatActivity {
         setResult(RESULT_OK, jumpingRopeIntent);
         super.onBackPressed();
     }
+
+    private void setHrefButtonBehavior() {
+        hrefButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri href = Uri.parse("http://www.aif.ru/health/secrets/trenirovka_so_skakalkoy_kak_prygat_pravilno");
+                Intent hrefIntent = new Intent(Intent.ACTION_VIEW, href);
+                Intent appChooser = new Intent().createChooser(hrefIntent, getString(R.string.app_chooser));
+                if (isAppInstalled("com.android.chrome")) {
+                    startActivity(appChooser);
+                } else {
+                    Toast.makeText(JumpingRopeActivity.this, R.string.chrome_browser_needed, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public boolean isAppInstalled(String targetPackage) {
+        List<ApplicationInfo> packages;
+        PackageManager pm = getPackageManager();
+        packages = pm.getInstalledApplications(0);
+        for (ApplicationInfo packageInfo : packages) {
+            if (packageInfo.packageName.equals(targetPackage)) return true;
+        }
+        return false;
+    }
+
 }
