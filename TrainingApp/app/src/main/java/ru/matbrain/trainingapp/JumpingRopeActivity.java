e20dfde21c03c791d66433a83ccb4ff6eabb1b67
Danz <danzjob@yandex.ru>
2017-06-04 15:57:15 +0300

    Добавлен таймер

--- TrainingApp/app/src/main/java/ru/matbrain/trainingapp/JumpingRopeActivity.java
+++ TrainingApp/app/src/main/java/ru/matbrain/trainingapp/JumpingRopeActivity.java
@@ -5,8 +5,10 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -22,25 +24,99 @@ import java.util.List;
 public class JumpingRopeActivity extends AppCompatActivity {
 
     private static final String TAG = "JumpingRopeActivity";
+    private static final String SECONDS_KEY = "SECONDS_KEY";
+    private static final String RUNNING_KEY = "RUNNING_KEY";
+    private static final String COUNT_KEY = "COUNT_KEY";
+    private static final String REPEAT_COUNT_KEY = "REPEAT_COUNT_KEY";
+    int seconds = 0;
+    boolean isRunning;
+    boolean wasRunning;
     private ImageButton minusButton;
     private ImageButton plusButton;
     private TextView jumpingRopeCountTV;
-
     private int jumpingRopeCount = 0;
-
     private Button hrefButton;
+    private Button exerciseDoneButton;
+    //    Поля таймера
+    private ImageButton startButton;
+    private ImageButton pauseButton;
+    private Button resetButton;
+    private TextView ClockTV;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jumping_rope);
+        Log.i(TAG, "Called method onCreate");
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt(SECONDS_KEY);
+            isRunning = savedInstanceState.getBoolean(RUNNING_KEY);
+            jumpingRopeCount = savedInstanceState.getInt(REPEAT_COUNT_KEY);
+        }
 
         initUI();
+        runTimer();
 
-        jumpingRopeCount = getIntent().getIntExtra(MainActivity.JUMPING_ROPE_KEY, 0);
+        if (getIntent() != null) {
+            jumpingRopeCount = getIntent().getIntExtra(MainActivity.JUMPING_ROPE_KEY, 0);
+        }
         jumpingRopeCountTV.setText(Integer.toString(jumpingRopeCount));
         setHrefButtonBehavior();
 
+    }
+
+    //    Сохраняем состояние счетчика повторений и таймера при изменении состояния жизненного цикла активности
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.i(TAG, "Called method onSaveInstanceState");
+        outState.putInt(SECONDS_KEY, seconds);
+        outState.putBoolean(RUNNING_KEY, isRunning);
+        outState.putInt(COUNT_KEY, jumpingRopeCount);
+//        outState.putInt();
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(TAG, "Called method onStart");
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "Called method onResume");
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "Called method onPause");
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "Called method onStop");
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.i(TAG, "Called method onRestart");
+
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "Called method onDestroy");
+
+        super.onDestroy();
     }
 
     private void initUI() {
@@ -50,7 +126,7 @@ public class JumpingRopeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (jumpingRopeCount >= 1) {
                     jumpingRopeCount--;
-                    jumpingRopeCountTV.setText(Integer.toString(jumpingRopeCount));
+                    jumpingRopeCountTV.setText(String.valueOf(jumpingRopeCount));
                 } else {
                     Toast.makeText(JumpingRopeActivity.this, "Can't reduce less than the current value", Toast.LENGTH_SHORT).show();
                 }
@@ -62,20 +138,63 @@ public class JumpingRopeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 jumpingRopeCount++;
-                jumpingRopeCountTV.setText(Integer.toString(jumpingRopeCount));
+                jumpingRopeCountTV.setText(String.valueOf(jumpingRopeCount));
             }
         });
         jumpingRopeCountTV = (TextView) findViewById(R.id.repeats_count);
 
         hrefButton = (Button) findViewById(R.id.href_jumping_rope);
+        exerciseDoneButton = (Button) findViewById(R.id.button_exercise_done);
+
+        setExerciseDoneButtonBehavior();
+
+        startButton = (ImageButton) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = true;
+            }
+        });
+        pauseButton = (ImageButton) findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = false;
+            }
+        });
+        resetButton = (Button) findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning) {
+                    isRunning = false;
+                    seconds = 0;
+                }
+            }
+        });
+        ClockTV = (TextView) findViewById(R.id.clock_text_view);
+
+
     }
 
     @Override
     public void onBackPressed() {
         Intent jumpingRopeIntent = new Intent();
         jumpingRopeIntent.putExtra(MainActivity.JUMPING_ROPE_KEY, jumpingRopeCount);
-        setResult(RESULT_OK, jumpingRopeIntent);
+        setResult(RESULT_CANCELED, jumpingRopeIntent);
         super.onBackPressed();
+    }
+
+    private void setExerciseDoneButtonBehavior() {
+        exerciseDoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent jumpingRopeIntent = new Intent();
+                jumpingRopeIntent.putExtra(MainActivity.JUMPING_ROPE_KEY, jumpingRopeCount);
+                setResult(RESULT_OK, jumpingRopeIntent);
+                finish();
+            }
+        });
     }
 
     private void setHrefButtonBehavior() {
@@ -104,4 +223,27 @@ public class JumpingRopeActivity extends AppCompatActivity {
         return false;
     }
 
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds / 3600) % 60;
+                int secs = seconds % 60;
+
+                String hoursStr = hours > 9 ? String.valueOf(hours) : String.valueOf("0" + hours);
+                String minutesStr = minutes > 9 ? String.valueOf(minutes) : String.valueOf("0" + minutes);
+                String secsStr = secs > 9 ? String.valueOf(secs) : String.valueOf("0" + secs);
+
+                String time = hoursStr + ":" + minutesStr + ":" + secsStr;
+                ClockTV.setText(time);
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
 }
